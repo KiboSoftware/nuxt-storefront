3de9d95f858293d447e9bf799ca587bea21781fc
Neha Bhanushali <Neha.Bhanushali@kibocommerce.com>
2021-10-14 12:41:21 +0530

    Removed unnecessary code from pages/index

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,4 @@
 <template>
-  <!-- <div>
-    <h1>Kibo Commerce</h1>
-    <button @click="showMessageFromBackend">Show message from backend</button>
-  </div>
-   -->
   <div>
     <TopBar />
     <AppHeader />
@@ -20,16 +15,5 @@ import BottomNavigation from "~/components/BottomNavigation.vue"
 
 export default {
   components: { AppHeader, TopBar, AppFooter, BottomNavigation },
-  methods: {
-    async showMessageFromBackend() {
-      /* eslint-disable no-console */
-      try {
-        const response = await this.$axios.get("/customerLogin")
-        console.log("customerLogin: ", response.data)
-      } catch (err) {
-        console.log(err)
-      }
-    },
-  },
 }
 </script>
