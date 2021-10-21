76a43b4114a6555ad17a74193373ca3e69353f05
kibo-sushant <89385472+kibo-sushant@users.noreply.github.com>
2021-10-21 06:46:37 +0530

    feat: Resolved all linting issues (#23)

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,9 @@
 <template>
-  <div>
-    <TopBar />
-    <AppHeader />
-    <AppFooter />
-    <BottomNavigation />
-  </div>
+  <div></div>
 </template>
 
 <script>
-import AppHeader from "~/components/AppHeader.vue"
-import TopBar from "~/components/TopBar.vue"
-import AppFooter from "~/components/AppFooter.vue"
-import BottomNavigation from "~/components/BottomNavigation.vue"
-
 export default {
-  components: { AppHeader, TopBar, AppFooter, BottomNavigation },
+  components: {},
 }
 </script>
--- server/api/product/configure.ts
+++ server/api/product/configure.ts
@@ -1,1 +1,4 @@
-export default (req, res) => "boo"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Request, Response } from "express"
+
+export default (req: Request, res: Response) => "boo"
