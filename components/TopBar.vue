9b920755b4f227bef16b0cfb8b95f4d2451d78d1
XPANXIONMAIL\Amol.Pawar <amol.pawar@kibocommerce.com>
2021-11-10 11:30:30 +0530

    bug(file): test

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -5,9 +5,7 @@
     </template>
     <template #center>
       <p>{{ $t("Download") }}</p>
-      <SfButton class="topbar__button sf-button--text">{{
-        $t("Find out more")
-      }}</SfButton>
+      <SfButton class="topbar__button sf-button--text">{{ $t("Find out more") }}</SfButton>
     </template>
     <template #right>
       <LocaleSelector />
