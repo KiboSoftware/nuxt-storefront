e4b4d93ddf29912798397c0844843e07c40f8de3
kibo-sushant <sushant.jadhav@blueconchtech.com>
2021-11-17 20:24:55 +0530

    feat: applyed style on PDP page

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,18 @@
     <TopBar />
     <AppHeader />
     <LoginModal />
-    <Nuxt />
+    <div class="content">
+      <Nuxt />
+    </div>
+
     <AppFooter />
     <BottomNavigation />
   </div>
 </template>
+
+<style>
+.content {
+  max-width: 81.75rem;
+  margin: 0 auto;
+}
+</style>
