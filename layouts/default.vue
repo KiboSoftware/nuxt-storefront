dd4971e709c5620a045aa524587e3c4f0c475844
amolpawar-kibo <92151681+amolpawar-kibo@users.noreply.github.com>
2021-11-25 00:33:59 +0530

    feat(files): shipping options custom component (#66)
    * feat(files): shipping options custom component
    
    * feat(files): resolved review commnets
    
    * feat(files): added default font-size, weight, line-height
    
    * feat(files): Roboto & sans-serif font applied

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,6 +13,13 @@
 </template>
 
 <style>
+html,
+body {
+  font-family: "Roboto", sans-serif;
+  font-size: 1rem;
+  line-height: 1.25rem;
+  font-weight: 400;
+}
 .content {
   max-width: 81.75rem;
   margin: 0 auto;
