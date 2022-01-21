63a2cc492474023b5ea4bc35d7b4fb781011fc82
Chandradeepta <43542673+Chandradeepta@users.noreply.github.com>
2022-01-21 19:17:13 +0530

    feat(fulfillmentOptions): PDP fulfillmentOptions implementation

--- plugins/modal.ts
+++ plugins/modal.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Vue from "vue"
 import { defineNuxtPlugin } from "#app"
 
@@ -9,6 +10,17 @@ export default defineNuxtPlugin((nuxtApp: any) => {
       this.subscription.$emit("open", {
         component,
         title,
+=======
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { defineNuxtPlugin } from "#app"
+import { ModalBus } from "@/eventBus/eventBus"
+
+export default defineNuxtPlugin((nuxtApp: any) => {
+  const modal = {
+    show: ({ component, props }) => {
+      ModalBus.$emit("open", {
+        component,
+>>>>>>> 01579bf (feat(fulfillmentOptions): PDP fulfillmentOptions implementation)
         props,
       })
     },
