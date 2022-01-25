aa7e0a5ae0bbf681b01f3aa7998b16d3e0f148b8
kibo-kevinwatts <85258296+kibo-kevinwatts@users.noreply.github.com>
2022-01-25 14:34:08 -0600

    Feat(content page): dynamic cms content pages  (#200)
    * feat(cms page): add route / page to handle cms pages with dynamic routes
    
    * fix(cms product carousel): adjust carousel spacing / fontsize
    
    * fix(layout vue): correct css on default layout

---
+++ pages/cms-page.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <KiboWidgetFactory :document-name="pageName" />
+  </div>
+</template>
+
+<script>
+import KiboWidgetFactory from "@/components/cms/KiboWidgetFactory.vue"
+import { useRoute } from "@nuxtjs/composition-api"
+export default {
+  components: { KiboWidgetFactory },
+  setup() {
+    const route = useRoute()
+    const { pageName } = route.value.params
+    return {
+      pageName,
+    }
+  },
+}
+</script>
