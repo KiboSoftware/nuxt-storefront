16b921ab116670256d498cc52cece5e54b6ab694
kibo-geetanshuchhabra <geetanshu.chhabra@kibocommerce.com>
2022-02-03 16:35:57 +0530

    remove comments from kiboskeleton.vue file

--- components/KiboSkeletonLoading.vue
+++ components/KiboSkeletonLoading.vue
@@ -31,53 +31,53 @@ export default {
 }
 
 .product-card-image {
-  border-radius: calc(var(--spacer-2xs) * 0.75); //3px;
-  width: calc(var(--spacer-base) * 8.375); // 201px
+  border-radius: calc(var(--spacer-2xs) * 0.75);
+  width: calc(var(--spacer-base) * 8.375);
   height: calc(var(--spacer-base) * 8.375);
   @include for-mobile {
     width: calc(var(--spacer-xl) * 3.25);
-    height: calc(var(--spacer-xl) * 3.75); //150px;
+    height: calc(var(--spacer-xl) * 3.75);
   }
 }
 
 .product-card-name {
-  border-radius: calc(var(--spacer-2xs) * 0.75); //3px;
+  border-radius: calc(var(--spacer-2xs) * 0.75);
   width: calc(var(--spacer-xl) * 2.175);
   height: calc(var(--spacer-xs) * 2.375);
   margin: 1rem 0 0.5rem 0;
 }
 
 .product-card-price {
-  border-radius: calc(var(--spacer-2xs) * 0.75); //3px;
+  border-radius: calc(var(--spacer-2xs) * 0.75);
   width: calc(var(--spacer-base) * 2);
   height: calc(var(--spacer-xs) * 2.375);
   margin: 0.5rem 0;
 }
 
 .product-card-rating {
-  border-radius: calc(var(--spacer-2xs) * 0.75); //3px;
+  border-radius: calc(var(--spacer-2xs) * 0.75);
   width: calc(var(--spacer-2xl) * 1.9);
   height: calc(var(--spacer-xs) * 2.375);
   margin: 0.5rem 0;
   @include for-mobile {
-    width: calc(var(--spacer-xl) * 3.25); //130px;
-    height: calc(var(--spacer-xs) * 2.25); //18px;
+    width: calc(var(--spacer-xl) * 3.25);
+    height: calc(var(--spacer-xs) * 2.25);
   }
 }
 
 .plp-category-name {
-  border-radius: calc(var(--spacer-2xs) * 0.75); //3px;
-  width: calc(var(--spacer-2xl) * 2.15); //172px;
+  border-radius: calc(var(--spacer-2xs) * 0.75);
+  width: calc(var(--spacer-2xl) * 2.15);
   height: calc(var(--spacer-xs) * 2.375);
   @include for-desktop {
-    width: calc(var(--spacer-2xl) * 2.15); //172px;
-    height: calc(var(--spacer-2xs) * 7); //28px;
+    width: calc(var(--spacer-2xl) * 2.15);
+    height: calc(var(--spacer-2xs) * 7);
   }
 }
 
 .plp-total-products-count {
-  border-radius: calc(var(--spacer-2xs) * 0.75); //3px;
-  width: calc(var(--spacer-xl) * 1.8); //72px;
+  border-radius: calc(var(--spacer-2xs) * 0.75);
+  width: calc(var(--spacer-xl) * 1.8);
   height: calc(var(--spacer-xs) * 2.375);
 }
 
@@ -85,18 +85,18 @@ export default {
   border: 1px solid #2b2b2b;
   background-color: #fff;
   border-radius: var(--spacer-2xs);
-  width: calc(var(--spacer-xl) * 3.7); //148px;
-  height: calc(var(--spacer-sm) * 2.125); //34px;
+  width: calc(var(--spacer-xl) * 3.7);
+  height: calc(var(--spacer-sm) * 2.125);
   @include for-desktop {
-    width: calc(var(--spacer-3xl) * 1.1875); //190px;
-    height: calc(var(--spacer-lg) * 1.0625); //34px;
+    width: calc(var(--spacer-3xl) * 1.1875);
+    height: calc(var(--spacer-lg) * 1.0625);
   }
 }
 
 .plp-sort-by {
-  border-radius: calc(var(--spacer-2xs) * 0.75); //3px;
-  width: calc(var(--spacer-sm) * 3.4375); //55px;
-  height: calc(var(--spacer-xl) * 0.5); //20px;
+  border-radius: calc(var(--spacer-2xs) * 0.75);
+  width: calc(var(--spacer-sm) * 3.4375);
+  height: calc(var(--spacer-xl) * 0.5);
 }
 
 @keyframes loading {
