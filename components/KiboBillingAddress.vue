00b1e274b3f6820905684d5436d707a4caabbc1b
XPANXIONMAIL\Amol.Pawar <amol.pawar@kibocommerce.com>
2022-02-17 15:44:07 +0530

    feat: refactore component

--- components/KiboBillingAddress.vue
+++ components/KiboBillingAddress.vue
@@ -8,86 +8,11 @@
         class="form__element form__checkbox"
         @change="$emit('same-as-shipping')"
       />
-      <SfInput
-        :label="$t('First Name')"
-        name="firstName"
-        class="form__element form__element--half"
-        required
-        :value="billingDetails.firstName"
-        @input="updateField('firstName', $event)"
-      />
-      <SfInput
-        :label="$t('Last Name')"
-        name="lastName"
-        class="form__element form__element--half form__element--half-even"
-        required
-        :value="billingDetails.lastNameOrSurname"
-        @input="updateField('lastNameOrSurname', $event)"
-      />
-      <SfInput
-        :label="$t('Street Address')"
-        name="streetName"
-        class="form__element form__element--half"
-        required
-        :value="billingDetails.address && billingDetails.address.address1"
-        @input="updateField('address1', $event)"
-      />
-      <SfInput
-        :label="$t('Apt')"
-        name="apartment"
-        class="form__element form__element--half form__element--half-even"
-        required
-        :value="billingDetails.address && billingDetails.address.address2"
-        @input="updateField('address2', $event)"
-      />
-      <SfInput
-        :label="$t('City')"
-        name="city"
-        class="form__element form__element--half"
-        required
-        :value="billingDetails.address && billingDetails.address.cityOrTown"
-        @input="updateField('cityOrTown', $event)"
-      />
-      <SfInput
-        :label="$t('State')"
-        name="state"
-        class="form__element form__element--half form__element--half-even"
-        required
-        :value="billingDetails.address && billingDetails.address.stateOrProvince"
-        @input="updateField('stateOrProvince', $event)"
-      />
-      <SfInput
-        :label="$t('Zip Code')"
-        name="zipCode"
-        class="form__element form__element--half"
-        required
-        :value="billingDetails.address && billingDetails.address.postalOrZipCode"
-        @input="updateField('postalOrZipCode', $event)"
-      />
-      <SfSelect
-        :label="$t('Country')"
-        :placeholder="`Select ${$t('Country')}`"
-        class="form__element form__element--half form__element--half-even form__select"
-        required
-        data-testid="country"
-        :value="billingDetails.address && billingDetails.address.countryCode"
-        @input="updateField('countryCode', $event)"
-      >
-        <SfSelectOption
-          v-for="countryOption in countries"
-          :key="countryOption"
-          :value="countryOption"
-        >
-          {{ countryOption }}
-        </SfSelectOption>
-      </SfSelect>
-      <SfInput
-        :label="$t('Phone Number')"
-        name="phone"
-        class="form__element"
-        required
-        :value="billingDetails.phoneNumbers && billingDetails.phoneNumbers.home"
-        @input="updateField('phoneNumbers', $event)"
+      <KiboAddressForm
+        :key="keyRefCopyAddress"
+        :value="billingDetails"
+        :countries="countries"
+        @addressData="updateBillingDetails"
       />
       <SfCheckbox
         v-model="saveBillingAddress"
@@ -101,13 +26,11 @@
 </template>
 
 <script lang="ts">
-import { SfInput, SfSelect, SfCheckbox } from "@storefront-ui/vue"
+import { SfCheckbox } from "@storefront-ui/vue"
 import { watch, ref } from "@nuxtjs/composition-api"
 export default {
   name: "KiboBillingAddress",
   components: {
-    SfInput,
-    SfSelect,
     SfCheckbox,
   },
   props: {
@@ -129,53 +52,33 @@ export default {
     const billingDetails = ref({ ...props.value })
     const sameAsShipping = ref(false)
     const saveBillingAddress = ref(false)
+    const keyRefCopyAddress = ref(1)
 
-    const updateField = (fieldName, fieldValue) => {
-      const address = [
-        "address1",
-        "address2",
-        "cityOrTown",
-        "stateOrProvince",
-        "postalOrZipCode",
-        "countryCode",
-      ]
-
-      if (fieldName === "phoneNumbers") {
-        billingDetails.value.phoneNumbers = {
-          ...billingDetails.value.phoneNumbers,
-          home: fieldValue,
-        }
-      } else if (address.includes(fieldName)) {
-        billingDetails.value.address = {
-          ...billingDetails.value.address,
-          [fieldName]: fieldValue,
-        }
-      } else {
-        billingDetails.value[fieldName] = fieldValue
-      }
-
-      context.emit("billingAddressData", {
-        ...billingDetails.value,
-      })
+    const updateBillingDetails = (newBillingDetails) => {
+      billingDetails.value = { ...newBillingDetails }
+      context.emit("billingAddressData", { ...newBillingDetails })
     }
     watch(sameAsShipping, () => {
       if (sameAsShipping.value) {
         Object.keys(props.shipping).forEach((key) => {
           billingDetails.value[key] = props.shipping[key]
         })
+        keyRefCopyAddress.value = 2
       } else {
         Object.keys(props.shipping).forEach((key) => {
           billingDetails.value[key] = ""
         })
+        keyRefCopyAddress.value = 3
       }
       context.emit("billingAddressData", { ...billingDetails.value })
     })
 
     return {
-      updateField,
       billingDetails,
       sameAsShipping,
       saveBillingAddress,
+      updateBillingDetails,
+      keyRefCopyAddress,
     }
   },
 }
--- components/KiboShipping.vue
+++ components/KiboShipping.vue
@@ -6,88 +6,11 @@
       class="sf-heading--left sf-heading--no-underline title"
     />
     <div class="form">
-      <SfInput
-        :label="$t('First Name')"
-        name="firstName"
-        class="form__element form__element--half"
-        required
-        :value="shippingAddress.firstName"
-        @input="updateField('firstName', $event)"
+      <KiboAddressForm
+        :value="shippingAddress"
+        :countries="countries"
+        @addressData="updateShippingDetails"
       />
-      <SfInput
-        :label="$t('Last Name')"
-        name="lastName"
-        class="form__element form__element--half form__element--half-even"
-        required
-        :value="shippingAddress.lastNameOrSurname"
-        @input="updateField('lastNameOrSurname', $event)"
-      />
-      <SfInput
-        :label="$t('Street Address')"
-        name="streetName"
-        class="form__element form__element--half"
-        required
-        :value="shippingAddress.address && shippingAddress.address.address1"
-        @input="updateField('address1', $event)"
-      />
-      <SfInput
-        :label="$t('Apt')"
-        name="apartment"
-        class="form__element form__element--half form__element--half-even"
-        required
-        :value="shippingAddress.address && shippingAddress.address.address2"
-        @input="updateField('address2', $event)"
-      />
-      <SfInput
-        :label="$t('City')"
-        name="city"
-        class="form__element form__element--half"
-        required
-        :value="shippingAddress.address && shippingAddress.address.cityOrTown"
-        @input="updateField('cityOrTown', $event)"
-      />
-      <SfInput
-        :label="$t('State')"
-        name="state"
-        class="form__element form__element--half form__element--half-even"
-        required
-        :value="shippingAddress.address && shippingAddress.address.stateOrProvince"
-        @input="updateField('stateOrProvince', $event)"
-      />
-      <SfInput
-        :label="$t('Zip Code')"
-        name="zipCode"
-        class="form__element form__element--half"
-        required
-        :value="shippingAddress.address && shippingAddress.address.postalOrZipCode"
-        @input="updateField('postalOrZipCode', $event)"
-      />
-      <SfSelect
-        :placeholder="`Select ${$t('Country')}`"
-        class="form__element form__element--half form__element--half-even form__select sf-select--underlined"
-        :label="$t('Country')"
-        :valid="true"
-        data-testid="country"
-        :value="shippingAddress.address && shippingAddress.address.countryCode"
-        @input="updateField('countryCode', $event)"
-      >
-        <SfSelectOption
-          v-for="countryOption in countries"
-          :key="countryOption"
-          :value="countryOption"
-        >
-          {{ countryOption }}
-        </SfSelectOption>
-      </SfSelect>
-      <SfInput
-        :label="$t('Phone Number')"
-        name="phone"
-        class="form__element"
-        required
-        :value="shippingAddress.phoneNumbers && shippingAddress.phoneNumbers.home"
-        @input="updateField('phoneNumbers', $event)"
-      />
-
       <SfButton @click="saveShippingAddress($event)">
         {{ $t("Save Shipping Address") }}
       </SfButton>
@@ -102,15 +25,13 @@
 </template>
 
 <script lang="ts">
-import { SfHeading, SfInput, SfButton, SfSelect } from "@storefront-ui/vue"
+import { SfHeading, SfButton } from "@storefront-ui/vue"
 
 export default {
   name: "KiboShipping",
   components: {
     SfHeading,
-    SfInput,
     SfButton,
-    SfSelect,
   },
   props: {
     value: {
@@ -124,29 +45,8 @@ export default {
   },
   setup(props, context) {
     const shippingAddress = ref({ ...props.value })
-    const updateField = (fieldName, fieldValue) => {
-      const address = [
-        "address1",
-        "address2",
-        "cityOrTown",
-        "stateOrProvince",
-        "postalOrZipCode",
-        "countryCode",
-      ]
-
-      if (fieldName === "phoneNumbers") {
-        shippingAddress.value.phoneNumbers = {
-          ...shippingAddress.value.phoneNumbers,
-          home: fieldValue,
-        }
-      } else if (address.includes(fieldName)) {
-        shippingAddress.value.address = {
-          ...shippingAddress.value.address,
-          [fieldName]: fieldValue,
-        }
-      } else {
-        shippingAddress.value[fieldName] = fieldValue
-      }
+    const updateShippingDetails = (newShippingDetails) => {
+      shippingAddress.value = { ...newShippingDetails }
     }
 
     const saveShippingAddress = () => {
@@ -156,9 +56,9 @@ export default {
     }
 
     return {
-      updateField,
       shippingAddress,
       saveShippingAddress,
+      updateShippingDetails,
     }
   },
 }
