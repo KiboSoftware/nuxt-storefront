9b1aadfe8997d7c69f62e6f96ea4710309cd053c
amolpawar-kibo <amol.pawar@kibocommerce.com>
2022-03-10 18:16:38 +0530

    feat(checkout): validation & bug fixes

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -76,6 +76,12 @@
       <div class="checkout__aside">
         <transition name="sf-fade">
           <SfLoader :loading="loading">
+            <!-- 
+              TODO - tobe added into KiboOrderSummary
+              :is-valid-coupon="isValidCoupon"
+              :invalid-coupon-error-text="invalidCouponErrorText"
+              :applied-coupons="appliedCoupons"
+              :are-coupons-applied="areCouponsApplied" -->
             <KiboOrderSummary
               v-if="currentStep <= 2"
               :order="getOrder"
@@ -86,10 +92,6 @@
               :standard-shipping="standardShipping"
               :estimated-tax="estimatedTax"
               :estimated-order-total="estimatedOrderTotal"
-              :is-valid-coupon="isValidCoupon"
-              :invalid-coupon-error-text="invalidCouponErrorText"
-              :applied-coupons="appliedCoupons"
-              :are-coupons-applied="areCouponsApplied"
             >
               <template #actions>
                 <SfButton
