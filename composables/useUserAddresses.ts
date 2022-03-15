622472cff9ca820c9be68349da3d06d1ef4ae163
amolpawar-kibo <amol.pawar@kibocommerce.com>
2022-03-15 22:21:19 +0530

    feat(checkout): resolved review comments - validation

--- composables/useUserAddresses.ts
+++ composables/useUserAddresses.ts
@@ -16,11 +16,11 @@ import type { Maybe, CustomerContact } from "@/server/types/GraphQL"
 export const useUserAddresses = () => {
   const nuxt = useNuxtApp()
   const fetcher = nuxt.nuxt2Context.$gqlFetch
-  const userShippingAddresses = useState<Maybe<Array<CustomerContactCollection>>>(
+  const userShippingAddresses = useState<Maybe<Array<CustomerContact>>>(
     `use-user-shipping-addresses`,
     () => []
   )
-  const userBillingAddresses = useState<Maybe<Array<CustomerContactCollection>>>(
+  const userBillingAddresses = useState<Maybe<Array<CustomerContact>>>(
     `use-user-billing-addresses`,
     () => []
   )
