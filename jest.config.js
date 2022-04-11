15e28ca14037c458d70449ed61255c1319b7b8a0
Kevin Watts <kevin.watts@kibocommerce.com>
2022-04-11 13:36:16 -0500

    Revert " chore: add testing library for vue"
    This reverts commit ec94025113d72ae2037617c8b3048e7e21fcf0e9.

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,6 @@ module.exports = {
     "^vue$": "vue/dist/vue.common.js",
     "^#app$": "@nuxt/bridge/dist/runtime/index.mjs",
     "^@nuxtjs/composition-api$": "@nuxt/bridge/dist/runtime/capi.legacy.mjs",
-    "^.+.(css|styl|less|sass|scss|png|svg|jpg|ttf|woff|woff2)$": "jest-transform-stub",
   },
   setupFilesAfterEnv: ["./jest.setup.js"],
   transform: {
@@ -20,11 +19,9 @@ module.exports = {
         plugins: ["@babel/plugin-transform-runtime"],
       },
     ],
-    ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
     ".*\\.(vue)$": "vue-jest",
   },
   collectCoverage: true,
-  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node", "vue"],
-  transformIgnorePatterns: ["node_modules/(?!@nuxtjs/style-resources|@nuxt/bridge|@storefront-ui)"],
+  transformIgnorePatterns: ["node_modules/(?!@nuxtjs/style-resources|@nuxt/bridge)"],
   testEnvironment: "jsdom",
 }
