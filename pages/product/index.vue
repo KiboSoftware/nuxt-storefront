41b2481c4cb817fe8235c061eb1f1777bbc673c2
kibo-chandradeeptalaha <89371824+kibo-chandradeeptalaha@users.noreply.github.com>
2022-04-12 15:28:13 +0530

    feat: adding price-range component and fixing megamenu css (#300)

--- pages/product/index.vue
+++ pages/product/index.vue
@@ -40,12 +40,19 @@
 
             <div class="product__price-and-rating">
               <div v-if="product && product.price">
-                <SfPrice
+                <KiboPrice
                   :regular="$n(productGetters.getPrice(product).regular, 'currency')"
                   :special="
                     product.price.salePrice &&
-                    $n(productGetters.getSalePrice(product).special, 'currency')
+                    $n(productGetters.getPrice(product).special, 'currency')
                   "
+                  class="kibo-collectedProduct__price"
+                />
+              </div>
+              <div v-if="product && product.priceRange && !product.price">
+                <KiboPriceRange
+                  :lower="productGetters.getPriceRange(product).lower"
+                  :upper="productGetters.getPriceRange(product).upper"
                 />
               </div>
 
@@ -254,7 +261,6 @@
 
 <script lang="ts">
 import {
-  SfPrice,
   SfRating,
   SfButton,
   SfBreadcrumbs,
@@ -286,7 +292,6 @@ export default defineComponent({
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Product",
   components: {
-    SfPrice,
     SfRating,
     SfButton,
     SfBreadcrumbs,
