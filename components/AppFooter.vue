2d33f36db44c6e2b6c65deb2b0cb35f57493bbce
TanmaySaraf <tanmay.saraf@kibocommerce.com>
2022-04-25 14:27:04 +0530

    Merging App Footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,121 +1,128 @@
 <template>
-  <SfFooter :column="4" multiple class="footer">
-    <SfFooterColumn :title="$t('About us')">
-      <SfList>
-        <SfListItem v-for="item in aboutUs" :key="item">
-          <SfMenuItem :label="$t(item)" />
-        </SfListItem>
-      </SfList>
-    </SfFooterColumn>
-    <SfFooterColumn :title="$t('Departments')">
-      <SfList>
-        <SfListItem v-for="item in departments" :key="item">
-          <SfMenuItem :label="$t(item)" />
-        </SfListItem>
-      </SfList>
-    </SfFooterColumn>
-    <SfFooterColumn :title="$t('Help')">
-      <SfList>
-        <SfListItem v-for="item in help" :key="item">
-          <SfMenuItem :label="$t(item)" />
-        </SfListItem>
-      </SfList>
-    </SfFooterColumn>
-    <SfFooterColumn :title="$t('Payment & Delivery')">
-      <SfList>
-        <SfListItem v-for="item in paymentsDelivery" :key="item">
-          <SfMenuItem :label="$t(item)" />
-        </SfListItem>
-      </SfList>
-    </SfFooterColumn>
-    <SfFooterColumn title="Social">
-      <div class="footer__socials">
-        <SfImage
-          v-for="item in social"
-          :key="item"
-          class="footer__social-image"
-          :src="'/icons/' + item + '.svg'"
-          :alt="item"
-          width="32"
-          height="32"
-        />
-      </div>
-    </SfFooterColumn>
-  </SfFooter>
+  <div v-if="dropzoneContent !== undefined">
+    <SfFooter :column="3" multiple class="footer">
+      <SfFooterColumn>
+        <p v-html="dropzoneContent.dropzones[3].rows[0].columns[0].widgets[0].config.body"></p>
+      </SfFooterColumn>
+      <SfFooterColumn>
+        <p v-html="dropzoneContent.dropzones[3].rows[0].columns[1].widgets[0].config.body"></p>
+      </SfFooterColumn>
+      <SfFooterColumn>
+        <p v-html="dropzoneContent.dropzones[3].rows[0].columns[2].widgets[0].config.body"></p>
+      </SfFooterColumn>
+      <SfFooterColumn>
+        <p v-html="dropzoneContent.dropzones[4].rows[0].columns[0].widgets[0].config.body"></p>
+      </SfFooterColumn>
+      <SfFooter :column="2" multiple class="footer">
+        <SfFooterColumn>
+          <p
+            v-html="
+              dropzoneContent.dropzones[4].rows[0].columns[1].rows[0].columns[0].widgets[0].config
+                .body
+            "
+          ></p>
+        </SfFooterColumn>
+        <SfFooterColumn>
+          <p
+            v-html="
+              dropzoneContent.dropzones[4].rows[0].columns[1].rows[0].columns[1].widgets[0].config
+                .body
+            "
+          ></p>
+        </SfFooterColumn>
+      </SfFooter>
+    </SfFooter>
+    <div class="copyrighttext">
+      <p>
+        <fa icon="copyright" />"2022"Mystic Outdoor v2. All Rights reserved. Kibo Software, Inc.
+      </p>
+    </div>
+  </div>
 </template>
-
 <script>
-import { SfFooter, SfList, SfImage, SfMenuItem } from "@storefront-ui/vue"
+import { SfFooter } from "@storefront-ui/vue"
+import { useAsync } from "@nuxtjs/composition-api"
+import { useDropzoneContent } from "@/composables"
 
 export default {
   components: {
     SfFooter,
-    SfList,
-    SfImage,
-    SfMenuItem,
   },
-  data() {
+  setup() {
+    const { dropzoneContent, loadProperties } = useDropzoneContent("Footer")
+
+    useAsync(async () => {
+      await Promise.all([
+        loadProperties({
+          documentListName: "pageTemplateContent@mozu",
+          filter: `name eq site-page`,
+        }),
+      ])
+    })
     return {
-      aboutUs: ["Who we are", "Quality in the details", "Customer Reviews"],
-      departments: ["Women fashion", "Men fashion", "Kidswear", "Home"],
-      help: ["Customer service", "Size guide", "Contact us"],
-      paymentsDelivery: ["Purchase terms", "Guarantee"],
-      social: ["facebook", "pinterest", "google", "twitter", "youtube"],
-      isMobile: false,
-      desktopMin: 1024,
+      dropzoneContent,
     }
   },
 }
 </script>
+<style lang="scss">
+$background: #000;
 
-<style lang="scss" scoped>
-.footer {
-  @include for-desktop {
-    margin-bottom: 0;
-  }
-
-  &__socials {
-    display: flex;
-    justify-content: space-between;
-    margin: 0 auto var(--spacer-lg);
-    padding: var(--spacer-base) var(--spacer-xl);
-    @include for-desktop {
-      justify-content: flex-start;
-      padding: var(--spacer-xs) 0;
-      margin: 0 auto;
-    }
-  }
-
-  &__social-image {
-    margin: 0 var(--spacer-2xs) 0 0;
-  }
+.container {
+  background: $background;
 }
 
 .sf-footer {
-  .sf-menu-item {
-    justify-content: flex-start;
-    font-family: var(--font-family--primary);
-    @include for-mobile {
-      padding-left: var(--spacer-base);
-    }
-
-    &__label {
-      color: var(--menu-item-label-color, var(--_c-light-secondary));
-    }
+  @include for-desktop {
+    background: $background;
+    border-top: none;
+    padding: 0 !important;
+    width: 100%;
   }
 
-  @include for-desktop {
-    border-top: var(--spacer-xs) solid var(--c-primary);
-    padding-bottom: 0;
-    margin-top: var(--spacer-2xl);
+  @include for-mobile {
+    padding: 0;
+    width: 100%;
   }
 
   &__container {
     margin: var(--spacer-sm);
     @include for-desktop {
-      max-width: 69rem;
+      color: $background;
+      background-color: $background;
+      max-width: 1040px;
       margin: 0 auto;
     }
+
+    @include for-mobile {
+      max-width: 100%;
+    }
   }
+}
+
+.sf-footer-column {
+  background: none;
+  padding: 10px;
+
+  &__content {
+    display: block !important;
+  }
+
+  &__title {
+    color: $background;
+    background-color: $background;
+    @include for-mobile {
+      display: none;
+    }
+  }
+}
+
+.copyrighttext {
+  position: relative;
+  overflow: hidden;
+  text-align: center;
+  height: 50px;
+  background-color: $background;
+  color: var(--c-white);
 }
 </style>
